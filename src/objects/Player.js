b6889fb37ddd796451df6ae794208407690bfd3a
monsieur-jonas <72392671+monsieur-jonas@users.noreply.github.com>
2021-06-05 21:18:07 +0200

    Update Player.js

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -170,7 +170,7 @@ class Player extends Phaser.Physics.Arcade.Sprite{
         },500);
         setTimeout(function () {
           Tableau.current.player.rechargeSonTir = false;
-        }, 200);
+        }, 2000);
         this.tirer = true;
       }
       else {
